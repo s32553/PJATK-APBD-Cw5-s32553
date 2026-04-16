f91a15d32c044bcc4605277a1fb8039a6a1e0a61
Piotr Zaremba-Czereyski <s32553@pjwstk.edu.pl>
2026-04-16 20:31:49 +0200

    Final

--- PJATK-APBD-Cw5-s32553/Program.cs
+++ PJATK-APBD-Cw5-s32553/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Rejestracja kontrolerów
+builder.Services.AddControllers();
+
+// (opcjonalnie, ale często wymagane na zajęciach)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger (do testów)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Mapowanie kontrolerów
+app.MapControllers();
+
+app.Run();
