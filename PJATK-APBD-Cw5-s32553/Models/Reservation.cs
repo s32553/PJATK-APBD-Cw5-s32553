cc8096a0dd1c8bbe1a8a0be2e39f85eee07de33a
Piotr Zaremba-Czereyski <s32553@pjwstk.edu.pl>
2026-04-16 19:53:53 +0200

    Add Reservation model

---
+++ PJATK-APBD-Cw5-s32553/Models/Reservation.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace YourNamespace.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+
+        [Required]
+        public string OrganizerName { get; set; }
+
+        [Required]
+        public string Topic { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+
+        public string Status { get; set; }
+    }
+}
